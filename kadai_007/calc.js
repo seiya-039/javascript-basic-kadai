91a5bd998ef6e7346650af686c9179d96da968da
seiya039 <xxseiyaxx1118@gmail.com>
2023-11-22 07:44:50 +0900

    初回コミット

---
+++ kadai_007/calc.js
@@ -0,0 +1,24 @@
+/*
+変数(num)が3の倍数の場合： “3の倍数です”
+変数(num)が5の倍数の場合： “5の倍数です”
+変数(num)が3と5の倍数の場合： “3と5の倍数です”
+それ以外の場合： 変数(num)を出力する
+*/
+
+let num=15;
+
+if ((num % 3 == 0) && (num % 5 == 0)) {
+    console.log('3と5の倍数です');
+}
+
+else if (num % 3 == 0) {
+    console.log('3の倍数です');
+}
+
+else if (num % 5 == 0) {
+    console.log('5の倍数です');
+}
+
+else  {
+    console.log(num);
+}
