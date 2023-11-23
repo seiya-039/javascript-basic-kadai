609658978bf219e9520892078be8a2f084f4ce6d
seiya039 <xxseiyaxx1118@gmail.com>
2023-11-23 22:24:07 +0900

    kadai_mdn

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var today = new Date();
+     console.log(today);
+  
+     console.log("年=" + today.getFullYear());
+     console.log("月=" + (today.getMonth()+1));
+     console.log("日=" + today.getDate());
+}
