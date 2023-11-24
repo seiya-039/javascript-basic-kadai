fb95ad4fdf4008e70b6c7ffea812e11b7ede063c
seiya039 <xxseiyaxx1118@gmail.com>
2023-11-24 20:58:59 +0900

    kadai_014

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -2,8 +2,8 @@
 // 2	引数の二乗の計算結果がコンソールに出力されている
 
 const double = (num) => {
-    return num * 2;
-  }
-  
-  // 関数の戻り値を出力する
-  console.log(double(50));
+    return num ** 2;
+}
+
+
+console.log(double(10));
