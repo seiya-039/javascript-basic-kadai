1781d76f48d9e7819294f8adaa1731ce86ea0ce9
seiya039 <xxseiyaxx1118@gmail.com>
2023-11-29 01:31:30 +0900

    kadai_020,021

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,4 +1,6 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
 btn.addEventListener('click', ()=> {
-    const text = document.getElementById('text');
-text.textContent  = "ボタンをクリックしました";
+    text.textContent  = "ボタンをクリックしました";
 });
